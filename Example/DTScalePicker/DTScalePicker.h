55adbc4496d2cfea404b867c36a91b52cd621b84
Daniel Tjuatja <daniel.tjuatja@gmail.com>
2016-03-29 16:04:10 +0800

    - Carthage integration -

---
+++ Example/DTScalePicker/DTScalePicker.h
@@ -0,0 +1,19 @@
+//
+//  DTScalePicker.h
+//  DTScalePicker
+//
+//  Created by Daniel Tjuatja on 29/3/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTScalePicker.
+FOUNDATION_EXPORT double DTScalePickerVersionNumber;
+
+//! Project version string for DTScalePicker.
+FOUNDATION_EXPORT const unsigned char DTScalePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTScalePicker/PublicHeader.h>
+
+
